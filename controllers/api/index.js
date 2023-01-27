f127e05b5aa81c5b42e02ece96dfd30113487229
lacnoskillz <kaikane3457@hotmail.com>
2023-01-26 19:54:49 -0600

    imported handlebards in server.js. got simple homepage get route working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 //kai
-const router = require('express').Router();
-//const userRoutes = require('./userRoutes');
-//const reviewsRoutes = require('./reviewsRoutes');
-//const restaurantsRoutes = require('./restaurantsRoutes');
+// const router = require('express').Router();
+// //const userRoutes = require('./userRoutes');
+// //const reviewsRoutes = require('./reviewsRoutes');
+// //const restaurantsRoutes = require('./restaurantsRoutes');
 
-router.use('/reviews', reviewsRoutes);
-router.use('/users', userRoutes);
-router.use('/restaurants', restaurantsRoutes);
+// router.use('/reviews', reviewsRoutes);
+// router.use('/users', userRoutes);
+// router.use('/restaurants', restaurantsRoutes);
 
 
-module.exports = router;
+// module.exports = router;
