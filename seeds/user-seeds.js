46c318b4d613d36c50c1a18266b948e0a171bb50
lacnoskillz <kaikane3457@hotmail.com>
2023-01-31 19:02:40 -0600

    added passwords to seeds and changed emails to be unique bacause of validation

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,27 +4,32 @@ const userData = [
   {
     id: 1,
     user_name: "Goose Bean",
-    email: "123@gmail.com"
+    email: "1234@gmail.com",
+    password: "password12345"
   },
   {
     id: 2,
     user_name: "Duck Butt",
-    email: "123@gmail.com"
+    email: "12345@gmail.com",
+    password: "password12345"
   },
   {
     id: 3,
     user_name: "Whiskey Peets",
-    email: "123@gmail.com"
+    email: "123456@gmail.com",
+    password: "password12345"
   },
   {
     id: 4,
     user_name: "Gavin Belson",
-    email: "123@gmail.com"
+    email: "1234567@gmail.com",
+    password: "password12345"
   },
   {
     id: 5,
     user_name: "Bruce Tubi",
-    email: "123@gmail.com"
+    email: "12345678@gmail.com",
+    password: "password12345"
   },
 ];
 
