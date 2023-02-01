f5c6abf94c41a9702740001d9a5942f919311182
lacnoskillz <kaikane3457@hotmail.com>
2023-01-31 21:54:42 -0600

    added individualHooks: true to user seed data. can now log in with seeded userdata

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
 //needs email column 
 const { Model, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection.js');
 
-class User extends Model { }
+
+class User extends Model { 
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
