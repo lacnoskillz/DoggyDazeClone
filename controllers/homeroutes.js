4bc0f64a1a2a59fa9158c8159db5d7176e793db7
lacnoskillz <kaikane3457@hotmail.com>
2023-01-31 18:56:33 -0600

    added password to user table and updated email to validate. Added hooks to hash personal data before saving to database

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-//const { Restaurant, User, Review } = require('../models');
+const { Restaurant, User, Review } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
--- public/js/login.js
+++ public/js/login.js
@@ -2,10 +2,10 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     
-    const email = document.querySelector('#user-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && password) {
       
       const response = await fetch('/api/users/login', {
         method: 'POST',
@@ -21,4 +21,4 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  document.getElementById('.login-form').addEventListener('submit', loginFormHandler);
+  document.getElementById('loginbtn').addEventListener('submit', loginFormHandler);
