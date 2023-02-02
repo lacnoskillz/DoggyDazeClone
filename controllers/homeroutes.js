da9faa203bf9785ee11c2f813892bcc45532b121
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-02-02 00:57:15 -0600

    Fix error bugs and add rating slider

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -28,7 +28,12 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Review }],
+      include: [{
+        model: Review,
+        include: [
+          {model: Restaurant}
+        ] 
+      }]
     });
 
     const user = userData.get({ plain: true });
--- public/js/reviews.js
+++ public/js/reviews.js
@@ -2,18 +2,16 @@ const newReviewHandler = async (event) => {
     event.preventDefault();
 
     const description = document.querySelector('#review').value.trim();
+    const rating = document.querySelector('#myRange').value
 
-
-    if (description) {
+    if (description && rating) {
         const restaurant_id = document.URL.split('/').at(-1);
-        //needs to be changed
-        let rating = 10
         console.log(description)
         console.log(restaurant_id)
         //send a POST request to the API endpoint
         const response = await fetch('/api/reviews', {
             method: 'POST',
-            body: JSON.stringify({ rating,description, restaurant_id }),
+            body: JSON.stringify({ rating, description, restaurant_id }),
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -25,7 +23,12 @@ const newReviewHandler = async (event) => {
         }
     }
 }
-    
-document
-  .querySelector('.new-review-form')
-  .addEventListener('submit', newReviewHandler);
+
+//wrap in an if so we dont get errors
+if (document.querySelector('.new-review-form')) {
+    document
+        .querySelector('.new-review-form')
+        .addEventListener('submit', newReviewHandler)
+};
+
+
