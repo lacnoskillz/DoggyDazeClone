2d138665e2b3eec65d6231f6c66148751a713e04
lacnoskillz <kaikane3457@hotmail.com>
2023-07-15 16:04:02 -0500

    connection reset

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,5 +1,5 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 // const sequelize = new Sequelize(
 //   process.env.DB_NAME,
@@ -12,11 +12,11 @@
 // );
 
 //need this to run on heroku
-let sequelize;
+ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
+ sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
