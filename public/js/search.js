9236605ee6dc97068c44054a18e3996ed8b9c319
lacnoskillz <kaikane3457@hotmail.com>
2023-07-19 14:37:04 -0500

    revamped the google maps iframe and utilize placesservice. users can now search for restaurants easyer within san antonio and the saved name contains more accurate info/spelling

--- public/js/search.js
+++ public/js/search.js
@@ -1,40 +1,64 @@
 const result = document.getElementById("result");
+let map;
 
 const searchHandler = async (event) => {
-    event.preventDefault();
-    
-    const search = document.querySelector('#search').value.trim();
-    
-  
-    if (search) {
-        let searchLink = `https://www.google.com/maps/embed/v1/place?key=AIzaSyC7zAtsP6T4gvbxxyZbASjxQjIKfdAlOcs&q=${search}+(Restaurant),San+Antonio+TX`
-        document.querySelector('#map-frame').setAttribute("src", searchLink)
-        //capitalSearch = search
-        result.innerHTML = `${search}`
-    }
-  };
-//AIzaSyC7zAtsP6T4gvbxxyZbASjxQjIKfdAlOcs
-const addRestaurantHandler = async (event) => {
-    
-    event.preventDefault();
-  
-    const restaurant_name = result.innerHTML;
-    
-    if (restaurant_name) {
-      const response = await fetch('/api/restaurants', {
-        method: 'POST',
-        body: JSON.stringify({ restaurant_name }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+  event.preventDefault();
+  const search = document.querySelector('#search').value.trim();
 
+  if (search) {
+    const placesService = new google.maps.places.PlacesService(map);
+
+    const request = {
+      query: `${search} Restaurant, San Antonio, TX`,
+      fields: ["name", "formatted_address", "geometry"],
+    };
+
+    placesService.findPlaceFromQuery(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK && results && results.length > 0) {
+        const restaurantName = results[0].name;
+        const restaurantAddress = results[0].formatted_address;
+
+        result.textContent = `${restaurantName} - ${restaurantAddress}`;
+
+        // Update the map's center to the selected restaurant location
+        const restaurantLocation = results[0].geometry.location;
+        map.setCenter(restaurantLocation);
+        map.fitBounds(results[0].geometry.viewport); // Adjust zoom based on viewport
+
+        // Optionally, you can set a maximum zoom level
+        // const maxZoomLevel = 16;
+        // map.setZoom(Math.min(maxZoomLevel, map.getZoom()));
+      } else {
+        result.textContent = "No restaurant found.";
+      }
+    });
+  }
+};
+
+const addRestaurantHandler = async (event) => {
+  event.preventDefault();
+  const restaurant_name = result.innerHTML;
+
+  if (restaurant_name) {
+    const response = await fetch('/api/restaurants', {
+      method: 'POST',
+      body: JSON.stringify({ restaurant_name }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// Initialize the map with San Antonio's coordinates
+map = new google.maps.Map(document.getElementById("map"), {
+  center: { lat: 29.4241, lng: -98.4936 }, // Coordinates for San Antonio
+  zoom: 12,
+});
 
 document
   .querySelector('.search-form')
@@ -43,3 +67,4 @@ document
 document
   .querySelector('.confirm')
   .addEventListener('submit', addRestaurantHandler);
+
