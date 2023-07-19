63f4e7ff8694907fff778c3edac3f83006549ffa
lacnoskillz <kaikane3457@hotmail.com>
2023-07-19 13:05:48 -0500

    added a functioning button all homepage so users can click to view restaurant

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,29 @@ function gotonewres(){
 if(searchBtn){
 searchBtn.addEventListener('click', gotonewres)
 }
+document.addEventListener('DOMContentLoaded', function() {
+  const viewresBtn = document.getElementsByClassName("singleresbtn");
+  
+
+  function viewres(event) {
+    const clickedElement = event.target;
+    
+    // Check if the clicked element has the class "singleresbtn"
+    if (clickedElement.classList.contains("singleresbtn")) {
+      // Get the parent container with the class "Singlerestaurant"
+      const parentContainer = clickedElement.closest(".Singlerestaurant");
+      
+      // Get the ID of the specific restaurant from the data attribute
+      const restaurantId = parentContainer.getAttribute("data-restaurant-id");
+      
+      // Redirect to the appropriate restaurant page based on the restaurant's ID.
+      window.location.href = `/restaurant/${restaurantId}`;
+    }
+  }
+// add event listener to all btns
+  if (viewresBtn) {
+    for (const btn of viewresBtn) {
+      btn.addEventListener('click', viewres);
+    }
+  }
+});
