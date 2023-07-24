790b97fea7802b7c7dea27e59d69be510f1973f1
lacnoskillz <kaikane3457@hotmail.com>
2023-07-24 14:53:48 -0500

    added encoder helper in server for handleing redering issues with handle bars and restaurants with names that have special chars like &

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ 
+  helpers: {
+  encodeURI: (value) => encodeURIComponent(value),
+}, 
+});
 
 const sess = {
   secret: 'Super secret secret',
